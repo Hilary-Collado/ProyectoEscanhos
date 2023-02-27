c3a9a1b931c5bb3bcdac18f31c7b60d36eda6f61
Hilary <hilarycollado1@gmail.com>
2023-02-27 13:35:36 -0400

    primer commit

---
+++ ProyectoEscanhos/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace ProyectoEscanhos
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.usuario = new System.Windows.Forms.TextBox();
+            this.contrasenha = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ingresar = new System.Windows.Forms.Button();
+            this.salir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(335, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(426, 456);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Sitka Small", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(451, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(347, 54);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Inicio  de Sesion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(460, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Usuario";
+            // 
+            // usuario
+            // 
+            this.usuario.BackColor = System.Drawing.Color.Ivory;
+            this.usuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.usuario.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usuario.Location = new System.Drawing.Point(464, 169);
+            this.usuario.Name = "usuario";
+            this.usuario.Size = new System.Drawing.Size(303, 26);
+            this.usuario.TabIndex = 4;
+            // 
+            // contrasenha
+            // 
+            this.contrasenha.BackColor = System.Drawing.Color.Ivory;
+            this.contrasenha.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.contrasenha.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contrasenha.Location = new System.Drawing.Point(464, 239);
+            this.contrasenha.Name = "contrasenha";
+            this.contrasenha.Size = new System.Drawing.Size(303, 26);
+            this.contrasenha.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(460, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 23);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Contraseña";
+            // 
+            // ingresar
+            // 
+            this.ingresar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(62)))), ((int)(((byte)(138)))));
+            this.ingresar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(62)))), ((int)(((byte)(138)))));
+            this.ingresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ingresar.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ingresar.ForeColor = System.Drawing.SystemColors.Control;
+            this.ingresar.Location = new System.Drawing.Point(533, 312);
+            this.ingresar.Name = "ingresar";
+            this.ingresar.Size = new System.Drawing.Size(153, 34);
+            this.ingresar.TabIndex = 7;
+            this.ingresar.Text = "Ingresar";
+            this.ingresar.UseVisualStyleBackColor = false;
+            this.ingresar.Click += new System.EventHandler(this.ingresar_Click);
+            // 
+            // salir
+            // 
+            this.salir.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(62)))), ((int)(((byte)(138)))));
+            this.salir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.salir.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salir.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(62)))), ((int)(((byte)(138)))));
+            this.salir.Location = new System.Drawing.Point(533, 362);
+            this.salir.Name = "salir";
+            this.salir.Size = new System.Drawing.Size(153, 34);
+            this.salir.TabIndex = 8;
+            this.salir.Text = "Salir";
+            this.salir.UseVisualStyleBackColor = false;
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Ivory;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.salir);
+            this.Controls.Add(this.ingresar);
+            this.Controls.Add(this.contrasenha);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.usuario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Inicio";
+            this.Text = "Inicio";
+            this.TransparencyKey = System.Drawing.Color.White;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox usuario;
+        private System.Windows.Forms.TextBox contrasenha;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button ingresar;
+        private System.Windows.Forms.Button salir;
+    }
+}
+
---
+++ ProyectoEscanhos/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProyectoEscanhos
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection("server=DESKTOP-43OTBI3\\SQLEXPRESS;database=escanhos; integrated security=true");
+
+        private void ingresar_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            try
+            {
+                string con = "select * from usuario where usuario='"+usuario.Text+"' and contrasena='" +contrasenha.Text+"'";
+                SqlCommand comando = new SqlCommand(con, connection);
+                SqlDataReader lector;
+                lector = comando.ExecuteReader();
+
+                if (lector.HasRows == true)
+                {
+                    MessageBox.Show("Bienvenido/a");
+                }
+                else
+                {
+                    MessageBox.Show("Usuario o contraseña incorrecta");
+                }
+                connection.Close();
+            }
+            catch(Exception exe)
+            {
+                MessageBox.Show(exe.ToString());
+            }
+            
+        }
+    }
+}
